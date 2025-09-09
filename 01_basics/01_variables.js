e9a60b399398d79d444c02cbdb0c48bd12302905
Soheb Akhtar <126268043+sohebakhtar12@users.noreply.github.com>
2025-09-09 09:14:50 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144555
+let accountEmail="sohebakhtar2001@gmail.com"
+var accountPassword="854116"
+accountCity="katihar"
+let accountState;
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+//number => 2 to 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null=> standalone value 
+//undefined =>
+//Symbol => unique
+let state;
+let age = 18
+let name = "soheb"
+
+console.log(typeof undefined);
+console.log(typeof null);
