6e565fc147665e0c39067f3e8c862742ab989026
Soheb Akhtar <126268043+sohebakhtar12@users.noreply.github.com>
2025-09-10 06:45:08 +0000

    string

---
+++ 01_basics/03_string.js
@@ -0,0 +1,27 @@
+const name = "soheb"
+const myAge = 22
+
+//console.log(name +  myAge )
+
+console.log(`Hello m name is ${name} and my age is ${myAge}`)
+
+const gameName =new String ('SohebAkhtar')
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('k'))
+console.log(gameName.slice(-11, 5))
+console.log(gameName.substring(0, 5))
+console.log(gameName.split('-'))
+
+
+const newEx="    soheb  "
+console.log(newEx.trim())
+
+const url = "https://sohebakhtar.com/soheb%20akhtar"
+console.log( url.replace ('%20', '_'))
+console.log(url.includes('soheb'))
+
+
+
