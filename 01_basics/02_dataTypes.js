93b69ea8653beea15d9a30eacf4826d53082034c
Soheb Akhtar <126268043+sohebakhtar12@users.noreply.github.com>
2025-09-14 05:52:53 +0000

    math function

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -9,5 +9,34 @@ let state;
 let age = 18
 let name = "soheb"
 
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+let score =123.2569
+let valueInNumber=Number(score)  //convert string to number 
+console.log(typeof score);
+
+
+const othernum=123.36985
+console.log (othernum.toPrecision(4))
+
+const hund=10000000
+console.log (hund.toLocaleString(`en-IN`))
+
+//==================================MATH=======================
+
+
+console.log (Math)
+console.log ("remove -ve value " + Math.abs(-10))
+console.log ("roundup " + Math.round(4.3))
+console.log ("ceil up " + Math.ceil(4.3))
+console.log ("floor value " + Math.floor(4.3))
+console.log ("minimum value " + Math.min(9,6,3,8,5,2))
+console.log ("maximum value " + Math.max(9,6,3,8,5,2))
+console.log(Math.random());
+console.log ("rendom value greter then 1 " + Math.random()*10+1)
+console.log ("flool random value " + Math.floor(Math.random()*10+1))
+
+
+
+
